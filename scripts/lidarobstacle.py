3252cf5590c95a4eb4164455c125068bb3c04d1f
Pito Salas <pitosalas@gmail.com>
2018-07-30 17:46:24 -0400

    little

--- scripts/lidarobstacle.py
+++ scripts/lidarobstacle.py
@@ -1,25 +1,22 @@
 import numpy as np
 
 class LidarObstacle():
-    def __init__(self, time, data, sofar=None):
-        self.data = self.focus(data)
-        self.time = time
-        self.instant_max = self.get_instant_max()
-        if (sofar is not None):
-            self.update_reading(sofar)
-
-    def get_instant_max(self):
-        return(np.percentile(self.data, 75))
-
-    def focus(self, data):
+    def __init__(self):
+        self.reading = 30
+        self.noise = 0
+        self.time = 0
+    
+    def measure(self, time, data, noise):
         left_part = np.array(data[360-15:359])
         right_part = np.array(data[0:15])
-        return(np.concatenate((left_part, right_part)))
+        focus_part = np.concatenate((left_part, right_part))
+        self.new_reading = np.percentile(focus_part, 25)
+        self.new_noise = noise
+        self.new_time = time
 
-    def update_reading(self, sofar):
-        self.value = (sofar.instant_max - self.instant_max)/2
-        self.elapsed = (sofar.time - self.time)
-        self.value_prime = self.value / self.elapsed
-
+    def update(self):
+        self.reading = (self.new_reading + self.reading)/2
+        self.noise = self.new_noise
+        self.time = self.new_time
 
     
--- scripts/lidarobstacle_test.py
+++ scripts/lidarobstacle_test.py
@@ -9,18 +9,30 @@ class MyFirstTests(unittest.TestCase):
         self.data1 = np.random.uniform(30, 31, size=360)
         self.data2 = np.random.uniform(31, 32, size=360)
         self.data3 = np.random.uniform(32, 33, size=360)
+        self.lo1 = LidarObstacle()
 
     def testData(self):
         self.assertEqual(len(self.data1), 360)
+    
+    def testMeasureOnce(self):
+        self.lo1.measure(1, self.data1, 0)
+        self.assertAlmostEqual(self.lo1.new_reading, 30.25, 0)
 
-    def testFocus(self):
-        lo1 = LidarObstacle(time=0, data=self.data1)
-        self.assertAlmostEqual(lo1.get_instant_max(), 30.75, places=0)
-
-    def testOnestep(self):
-        lo1 = LidarObstacle(time=0, data=self.data1)
-        onestep = LidarObstacle(time=1, data=self.data2, sofar=lo1)
-        self.assertAlmostEqual(onestep.value_prime, 0.5, places=0)
+    def testTwice(self):
+        self.lo1.measure(1, self.data1, 0)
+        self.lo1.update()
+        self.lo1.measure(2, self.data2, 0)
+        self.lo1.update()
+        self.assertAlmostEqual(self.lo1.reading, 30.75, 0)
+    
+    def testThrice(self):
+        self.lo1.measure(1, self.data1, 0)
+        self.lo1.update()
+        self.lo1.measure(2, self.data2, 0)
+        self.lo1.update()
+        self.lo1.measure(3, self.data3, 0)
+        self.lo1.update()
+        self.assertAlmostEqual(self.lo1.reading, 31.5, 0)
 
 if __name__ == '__main__':
     unittest.main()
